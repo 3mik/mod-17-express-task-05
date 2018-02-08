04e78f42f19796f64e0ea36cb80d1420e58bc0b4
Krzysztof Milek <krzysztof.mile73k@gmail.com>
2018-02-08 20:25:01 +0100

    Complete task

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'GOOGLE_CLIENT_ID': '264037589739-j66k73chri47vcn5i4t1hip51h4k4hsi.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': '6NabpR15L6InRQzQpkUHTt1J',
+    'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
